372065db83cfa880e73ca8268bbb466d10f18c2f
Darius Ionatan Pop <Darius.Pop1@ibm.com>
2022-09-10 17:20:18 +0300

    base

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "hello from server" });
+});
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
